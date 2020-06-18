9a2a0bd44d4cb87c6582bbf83095e259ddce71ef
Pedro Henrique Cardozo Ferreira <p-cardozo@hotmail.com>
2020-06-18 15:59:36 -0300

    Add Prittier Version Files

--- path.js
+++ path.js
@@ -1,2 +1,2 @@
-const path = require('path')
-const server = require(path.join(__dirname, 'app', 'server.js'))
+const path = require("path");
+const server = require(path.join(__dirname, "app", "server.js"));
